ada3e5c80013fddfacb6ea20452ff229ae242f55
DavideBITMEX <davidebittelli@gmail.com>
2025-01-25 00:10:54 +0100

    n

--- R/app.R
+++ R/app.R
@@ -1,6 +1,20 @@
-# Load necessary libraries
 library(shiny)
-library(golem)
+library(ggplot2)
+library(DT)
 
-# Run the app
-run_app()
+ui <- fluidPage(
+  plotOutput("plot", brush = "plot_brush"),
+  DTOutput("table")
+)
+
+server <- function(input, output) {
+  output$plot <- renderPlot(
+    ggplot(mtcars) +
+      geom_point(aes(x = mpg, y = disp))
+  )
+  output$table <- renderDT({
+    brushedPoints(mtcars, input$plot_brush)
+  })
+}
+
+shinyApp(ui = ui, server = server)
