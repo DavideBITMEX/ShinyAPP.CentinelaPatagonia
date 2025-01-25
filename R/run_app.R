e4c77e6d94dcfa4b8820952081d6050769d37a3c
DavideBITMEX <davidebittelli@gmail.com>
2025-01-25 09:17:59 +0100

    hv

--- R/run_app.R
+++ R/run_app.R
@@ -11,22 +11,22 @@
 library(shinylive)
 library(httpuv)
 
-run_app <- function(
-  onStart = NULL,
-  options = list(),
-  enableBookmarking = NULL,
-  uiPattern = "/",
-  ...
-) {
-  with_golem_options(
-    app = shinyApp(
-      ui = app_ui,
-      server = app_server,
-      onStart = onStart,
-      options = options,
-      enableBookmarking = enableBookmarking,
-      uiPattern = uiPattern
-    ),
-    golem_opts = list(...)
-  )
-}
+# run_app <- function(
+#   onStart = NULL,
+#   options = list(),
+#   enableBookmarking = NULL,
+#   uiPattern = "/",
+#   ...
+# ) {
+#   with_golem_options(
+#     app = shinyApp(
+#       ui = app_ui,
+#       server = app_server,
+#       onStart = onStart,
+#       options = options,
+#       enableBookmarking = enableBookmarking,
+#       uiPattern = uiPattern
+#     ),
+#     golem_opts = list(...)
+#   )
+# }
