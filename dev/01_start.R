668f6bbac00bdd9e85f027acf8e07a8833270885
DavideBITMEX <davidebittelli@gmail.com>
2025-03-27 00:07:23 +0100

    description file

--- dev/01_start.R
+++ dev/01_start.R
@@ -22,9 +22,8 @@ golem::fill_desc(
   pkg_name = "CentinelaPatagonia", # The Name of the package containing the App
   pkg_title = "PKG_TITLE", # The Title of the package containing the App
   pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  authors = person(given = "Davide", family = "Bittelli", email = "davidebittelli@gmail.com", role = NULL, comment =
+                    NULL),
   repo_url = NULL, # The URL of the GitHub Repo (optional),
   pkg_version = "0.0.0.9000" # The Version of the package containing the App
 )
